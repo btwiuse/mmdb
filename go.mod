acfa1286aece185009122c3eaafc037bc0a06403
navigaid <navigaid@gmail.com>
2024-03-28 21:06:47 +0000

    add https://github.com/maxmind/mmdbinspect/blob/main/cmd/mmdbinspect/main.go

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/btwiuse/mmdb
+
+go 1.22.1
+
+require (
+	github.com/btwiuse/tags v0.0.2
+	github.com/maxmind/mmdbinspect v0.2.0
+)
+
+require (
+	github.com/oschwald/maxminddb-golang v1.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/btwiuse/tags"
+	"github.com/maxmind/mmdbinspect/pkg/mmdbinspect"
+)
+
+func usage() {
+	fmt.Printf(
+		"Usage: %s [-include-aliased-networks] -db path/to/db -db path/to/other/db 130.113.64.30/24 0:0:0:0:0:ffff:8064:a678\n", //nolint: lll
+		os.Args[0],
+	)
+	flag.PrintDefaults()
+	fmt.Print(`
+Any additional arguments passed are assumed to be networks to look up.  If an
+address range is not supplied, /32 will be assumed for ipv4 addresses and /128
+will be assumed for ipv6 addresses.
+`)
+}
+
+func main() {
+	var mmdb tags.CommaSeparatedStrings
+
+	flag.Var(&mmdb, "db", "Path to an mmdb file. You may pass this arg more than once.")
+	includeAliasedNetworks := flag.Bool(
+		"include-aliased-networks", false,
+		"Include aliased networks (e.g. 6to4, Teredo). This option may cause IPv4 networks to be listed more than once via aliases.", //nolint: lll
+	)
+
+	flag.Usage = usage
+	flag.Parse()
+
+	// Any remaining arguments (not passed via flags) should be networks
+	network := flag.Args()
+
+	if len(network) == 0 {
+		fmt.Println("You must provide at least one network address")
+		usage()
+		os.Exit(1)
+	}
+
+	if len(mmdb) == 0 {
+		fmt.Println("You must provide a path to at least one .mmdb file")
+		usage()
+		os.Exit(1)
+	}
+
+	records, err := mmdbinspect.AggregatedRecords(network, mmdb, *includeAliasedNetworks)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json, err := mmdbinspect.RecordToString(records)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v\n", json)
+}
